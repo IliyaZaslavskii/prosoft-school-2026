212d4e54cd0188651309cb86f66eb50cf704534f
Iliya Zaslavskii <102456608+IliyaZaslavskii@users.noreply.github.com>
2026-09-16 21:16:38 +0500

    Add RotateImage solution

---
+++ RotateImage/RotateImage/RotateImage.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        vector<vector<int>> result(rows, vector<int>(rows));
+        // Проходим по всем строкам исходной матрицы 
+        for (int i = rows - 1; i >= 0; --i) {
+            // Проходим по всем столбцам исходной матрицы
+            for (int j = 0; j < rows; ++j) {
+                // Переворачиваем матрицу
+                result[j][rows - 1 - i] = matrix[i][j];
+            }
+        }
+        // Возвращаем изменение
+        matrix = result;
+    }
+};
