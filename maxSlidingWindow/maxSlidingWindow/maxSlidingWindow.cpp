315c1a819b1f720de27d0b2334ae76fbd95b2d2a
Iliya Zaslavskii <102456608+IliyaZaslavskii@users.noreply.github.com>
2026-09-17 20:13:01 +0500

    Add maxSlidingWindow solution

---
+++ maxSlidingWindow/maxSlidingWindow/maxSlidingWindow.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        // Используем двухсторонную очередь, храним в ней индексы
+        deque<int> dq;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            // Удаляем элементы, вышедшие за границы окна
+            if (!dq.empty() && dq.front() < i - k + 1) {
+                dq.pop_front();
+            };
+            // Удаляем элементы с конца, которые меньше текущего
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+            // Добавляем текущий индекс
+            dq.push_back(i);
+
+            // Когда окно полное, записываем максимум
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+
+        return result;
+    }
+};
